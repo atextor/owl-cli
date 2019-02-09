d5d4f37c643414da10c7250bb222606dac008d9f
Andreas Textor <textor.andreas@googlemail.com>
2019-02-09 13:23:33 +0100

    Move diagram generation to DiagramGenerator

--- src/main/java/de/atextor/owldiagram/App.java
+++ src/main/java/de/atextor/owldiagram/App.java
@@ -1,80 +1,21 @@
 package de.atextor.owldiagram;
 
+import de.atextor.owldiagram.diagram.DiagramGenerator;
 import de.atextor.owldiagram.diagram.GraphvizDocument;
-import de.atextor.owldiagram.diagram.GraphvizGenerator;
-import de.atextor.owldiagram.graph.GraphElement;
-import de.atextor.owldiagram.mappers.OWLAxiomMapper;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 public class App {
-    private static void writeToStdout( final InputStream in ) throws IOException {
-        final byte[] buffer = new byte[1024];
-        for ( ; ; ) {
-            final int bytesRead = in.read( buffer );
-            if ( bytesRead == -1 ) {
-                break;
-            }
-            System.out.write( buffer, 0, bytesRead );
-        }
     }
 
-    private static void executeDot( final Consumer<OutputStream> contentProvider, final String outputFile,
-                                    final File workingDir ) {
-        try {
-            final boolean writeToStdout = outputFile.equals( "-" );
-            final String output = writeToStdout ? "" : " -o" + outputFile;
-            final String command = "dot -Tsvg" + output;
-            final Process process;
-            process = Runtime.getRuntime().exec( command, null, workingDir );
-            final OutputStream processStdIn = process.getOutputStream();
-            final InputStream processStdOut = process.getInputStream();
+    public static void main( final String[] args ) {
 
-            contentProvider.accept( processStdIn );
-
-            if ( writeToStdout ) {
-                writeToStdout( processStdOut );
-            }
-
-            process.waitFor();
-        } catch ( final IOException exception ) {
-            System.err.println( "Error while running dot: " + exception.getMessage() );
-        } catch ( final InterruptedException exception ) {
-            System.err.println( "Command interrupted: " + exception.getMessage() );
-        }
-    }
-
-    public static void main( final String[] args ) throws OWLOntologyCreationException {
-        final OWLOntologyManager m = OWLManager.createOWLOntologyManager();
-        final OWLOntology ontology = m.loadOntologyFromOntologyDocument( App.class.getResourceAsStream( "/test.owl" ) );
-
-        final OWLAxiomMapper visitor = new OWLAxiomMapper();
-        final GraphvizGenerator graphvizGenerator = new GraphvizGenerator();
-
-        final Stream<GraphElement> graphElements = ontology.axioms().flatMap( axiom -> axiom.accept( visitor ) );
-        final GraphvizDocument graphvizDocument = graphvizGenerator.apply( graphElements );
-        final String result = graphvizDocument.apply( GraphvizDocument.DEFAULT_CONFIGURATION );
-
-        final Consumer<OutputStream> contentProvider = outputStream -> {
-            try {
-                outputStream.write( result.getBytes() );
-                outputStream.flush();
-                outputStream.close();
-            } catch ( final IOException exception ) {
-                System.err.println( "Error while writing to output: " + exception.getMessage() );
-            }
-        };
 
         final File workingDir = new File( "/home/tex/git/owl-diagram/src/main/resources/" );
-        executeDot( contentProvider, "/home/tex/git/owl-diagram/src/main/resources/test.svg", workingDir );
+        final String output = "/home/tex/git/owl-diagram/src/main/resources/test.svg";
+
+        final DiagramGenerator generator = new DiagramGenerator();
+        generator.generate( App.class.getResourceAsStream( "/test.owl" ), GraphvizDocument.DEFAULT_CONFIGURATION,
+                output, workingDir );
     }
 }
--- src/main/java/de/atextor/owldiagram/diagram/DiagramGenerator.java
+++ src/main/java/de/atextor/owldiagram/diagram/DiagramGenerator.java
@@ -2,39 +2,86 @@ package de.atextor.owldiagram.diagram;
 
 import de.atextor.owldiagram.graph.GraphElement;
 import de.atextor.owldiagram.mappers.OWLAxiomMapper;
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.model.MutableGraph;
-import guru.nidi.graphviz.parse.Parser;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class DiagramGenerator {
-    private final OWLAxiomMapper visitor;
-    private final GraphvizGenerator graphvizGenerator;
+    private final OWLAxiomMapper visitor = new OWLAxiomMapper();
+    private final GraphvizGenerator graphvizGenerator = new GraphvizGenerator();
 
-    public DiagramGenerator() {
-        visitor = new OWLAxiomMapper();
-        graphvizGenerator = new GraphvizGenerator();
+    private void writeToStdout( final InputStream in ) throws IOException {
+        final byte[] buffer = new byte[1024];
+        for ( ; ; ) {
+            final int bytesRead = in.read( buffer );
+            if ( bytesRead == -1 ) {
+                break;
+            }
+            System.out.write( buffer, 0, bytesRead );
+        }
     }
 
-    public void generate( final OWLOntology ontology, final OutputStream outputStream ) {
-        generate( ontology, GraphvizDocument.DEFAULT_CONFIGURATION, outputStream );
-    }
-
-    public void generate( final OWLOntology ontology, final Configuration configuration,
-                          final OutputStream outputStream ) {
-        final Stream<GraphElement> graphElements = ontology.axioms().flatMap( axiom -> axiom.accept( visitor ) );
-        final GraphvizDocument graphvizDocument = graphvizGenerator.apply( graphElements );
-
+    private void executeDot( final Consumer<OutputStream> contentProvider, final String outputFile,
+                             final File workingDir ) {
         try {
-            final MutableGraph graph = Parser.read( graphvizDocument.apply( configuration ) );
-            Graphviz.fromGraph( graph ).render( Format.SVG ).toOutputStream( outputStream );
-        } catch ( final IOException e ) {
+            final boolean writeToStdout = outputFile.equals( "-" );
+            final String output = writeToStdout ? "" : " -o" + outputFile;
+            final String command = "dot -Tsvg" + output;
+            final Process process;
+            process = Runtime.getRuntime().exec( command, null, workingDir );
+            final OutputStream processStdIn = process.getOutputStream();
+            final InputStream processStdOut = process.getInputStream();
+
+            contentProvider.accept( processStdIn );
+
+            if ( writeToStdout ) {
+                writeToStdout( processStdOut );
+            }
+
+            process.waitFor();
+        } catch ( final IOException exception ) {
+            System.err.println( "Error while running dot: " + exception.getMessage() );
+        } catch ( final InterruptedException exception ) {
+            System.err.println( "Command interrupted: " + exception.getMessage() );
+        }
+    }
+
+    public void generate( final InputStream ontologyInputStream, final Configuration configuration, final String output,
+                          final File workingDir ) {
+        final OWLOntologyManager m = OWLManager.createOWLOntologyManager();
+        final OWLOntology ontology;
+        try {
+            ontology = m.loadOntologyFromOntologyDocument( ontologyInputStream );
+            generate( ontology, configuration, output, workingDir );
+        } catch ( final OWLOntologyCreationException e ) {
             e.printStackTrace();
         }
+    }
+
+    public void generate( final OWLOntology ontology, final Configuration configuration, final String output,
+                          final File workingDir ) {
+        final Stream<GraphElement> graphElements = ontology.axioms().flatMap( axiom -> axiom.accept( visitor ) );
+        final GraphvizDocument graphvizDocument = graphvizGenerator.apply( graphElements );
+        final String graphvizGraph = graphvizDocument.apply( configuration );
+
+        final Consumer<OutputStream> contentProvider = outputStream -> {
+            try {
+                outputStream.write( graphvizGraph.getBytes() );
+                outputStream.flush();
+                outputStream.close();
+            } catch ( final IOException exception ) {
+                System.err.println( "Error while writing to output: " + exception.getMessage() );
+            }
+        };
+
+        executeDot( contentProvider, output, workingDir );
     }
 }
