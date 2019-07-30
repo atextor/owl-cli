4e9b849c88a88662467329065c56892017b77f6b
Andreas Textor <textor.andreas@googlemail.com>
2019-07-30 05:59:45 +0200

    Add test for OWLObjectPropertyAssertion

--- src/test/java/de/atextor/owldiagram/OWLAxiomMapperTest.java
+++ src/test/java/de/atextor/owldiagram/OWLAxiomMapperTest.java
@@ -15,6 +15,8 @@ import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
 import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
 import org.semanticweb.owlapi.model.OWLEquivalentDataPropertiesAxiom;
 import org.semanticweb.owlapi.model.OWLEquivalentObjectPropertiesAxiom;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
@@ -23,6 +25,8 @@ import uk.ac.manchester.cs.owl.owlapi.OWLDataPropertyImpl;
 import uk.ac.manchester.cs.owl.owlapi.OWLEquivalentClassesAxiomImpl;
 import uk.ac.manchester.cs.owl.owlapi.OWLEquivalentDataPropertiesAxiomImpl;
 import uk.ac.manchester.cs.owl.owlapi.OWLEquivalentObjectPropertiesAxiomImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLNamedIndividualImpl;
+import uk.ac.manchester.cs.owl.owlapi.OWLObjectPropertyAssertionAxiomImpl;
 import uk.ac.manchester.cs.owl.owlapi.OWLObjectPropertyImpl;
 import uk.ac.manchester.cs.owl.owlapi.OWLSubObjectPropertyOfAxiomImpl;
 
@@ -85,12 +89,12 @@ public class OWLAxiomMapperTest extends MapperTestBase {
         final IRI fooIri = IRI.create( "http://test.de#foo" );
         final IRI barIri = IRI.create( "http://test.de#bar" );
         final IRI bazIri = IRI.create( "http://test.de#baz" );
-        final OWLObjectPropertyExpression dataPropertyExpression1 = new OWLObjectPropertyImpl( fooIri );
-        final OWLObjectPropertyExpression dataPropertyExpression2 = new OWLObjectPropertyImpl( barIri );
-        final OWLObjectPropertyExpression dataPropertyExpression3 = new OWLObjectPropertyImpl( bazIri );
+        final OWLObjectPropertyExpression objectPropertyExpression1 = new OWLObjectPropertyImpl( fooIri );
+        final OWLObjectPropertyExpression objectPropertyExpression2 = new OWLObjectPropertyImpl( barIri );
+        final OWLObjectPropertyExpression objectPropertyExpression3 = new OWLObjectPropertyImpl( bazIri );
         final OWLEquivalentObjectPropertiesAxiom axiom =
-                new OWLEquivalentObjectPropertiesAxiomImpl( Arrays.asList( dataPropertyExpression1,
-                        dataPropertyExpression2, dataPropertyExpression3 ), Collections.emptyList() );
+            new OWLEquivalentObjectPropertiesAxiomImpl( Arrays.asList( objectPropertyExpression1,
+                objectPropertyExpression2, objectPropertyExpression3 ), Collections.emptyList() );
 
         final List<GraphElement> result = mapper.visit( axiom ).collect( Collectors.toList() );
 
@@ -119,6 +123,39 @@ public class OWLAxiomMapperTest extends MapperTestBase {
 
     @Test
     public void testOWLObjectPropertyAssertionAxiom() {
+        final IRI fooIri = IRI.create( "http://test.de#foo" );
+        final IRI barIri = IRI.create( "http://test.de#bar" );
+        final IRI propertyIri = IRI.create( "http://test.de#property" );
+
+        final OWLIndividual foo = new OWLNamedIndividualImpl( fooIri );
+        final OWLIndividual bar = new OWLNamedIndividualImpl( barIri );
+        final OWLObjectPropertyExpression property = new OWLObjectPropertyImpl( propertyIri );
+        final OWLObjectPropertyAssertionAxiom axiom = new OWLObjectPropertyAssertionAxiomImpl( foo,
+            property, bar, Collections.emptyList() );
+
+        final String invisibleId = "invisibleNode";
+        testIdentifierMapper.pushAnonId( new Node.Id( invisibleId ) );
+
+        final List<GraphElement> result = mapper.visit( axiom ).collect( Collectors.toList() );
+        assertThat( result ).hasSize( 7 );
+
+        final List<Node> nodes = nodes( result );
+        assertThat( nodes ).hasSize( 4 );
+        assertThat( nodes ).anyMatch( isNodeWithId( "foo" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "bar" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "property" ) );
+        assertThat( nodes ).anyMatch( isInvisible() );
+
+        final List<Edge> edges = edges( result );
+        assertThat( edges ).hasSize( 3 );
+
+        final Edge fooToInvis = edges.stream().filter( isEdgeWithFromAndTo( "foo", invisibleId ) ).findAny().get();
+        final Edge invisToBar = edges.stream().filter( isEdgeWithFromAndTo( invisibleId, "bar" ) ).findAny().get();
+        final Edge invisToProp =
+            edges.stream().filter( isEdgeWithFromAndTo( invisibleId, "property" ) ).findAny().get();
+        assertThat( fooToInvis.getType() ).isEqualTo( Edge.Type.NO_ARROW );
+        assertThat( invisToBar.getType() ).isEqualTo( Edge.Type.DEFAULT_ARROW );
+        assertThat( invisToProp.getType() ).isEqualTo( Edge.Type.DASHED_ARROW );
     }
 
     @Test
@@ -132,7 +169,7 @@ public class OWLAxiomMapperTest extends MapperTestBase {
         final OWLObjectPropertyExpression foo = new OWLObjectPropertyImpl( fooIri );
         final OWLObjectPropertyExpression bar = new OWLObjectPropertyImpl( barIri );
         final OWLSubObjectPropertyOfAxiom axiom = new OWLSubObjectPropertyOfAxiomImpl( foo, bar,
-                Collections.emptyList() );
+            Collections.emptyList() );
 
         final List<GraphElement> result = mapper.visit( axiom ).collect( Collectors.toList() );
         assertThat( result ).hasSize( 3 );
@@ -175,8 +212,8 @@ public class OWLAxiomMapperTest extends MapperTestBase {
         final OWLDataPropertyExpression dataPropertyExpression2 = new OWLDataPropertyImpl( barIri );
         final OWLDataPropertyExpression dataPropertyExpression3 = new OWLDataPropertyImpl( bazIri );
         final OWLEquivalentDataPropertiesAxiom axiom =
-                new OWLEquivalentDataPropertiesAxiomImpl( Arrays.asList( dataPropertyExpression1,
-                        dataPropertyExpression2, dataPropertyExpression3 ), Collections.emptyList() );
+            new OWLEquivalentDataPropertiesAxiomImpl( Arrays.asList( dataPropertyExpression1,
+                dataPropertyExpression2, dataPropertyExpression3 ), Collections.emptyList() );
 
         final List<GraphElement> result = mapper.visit( axiom ).collect( Collectors.toList() );
 
@@ -211,7 +248,7 @@ public class OWLAxiomMapperTest extends MapperTestBase {
         final OWLClassExpression classExpression2 = new OWLClassImpl( barIri );
         final OWLClassExpression classExpression3 = new OWLClassImpl( bazIri );
         final OWLEquivalentClassesAxiom axiom = new OWLEquivalentClassesAxiomImpl( Arrays.asList( classExpression1,
-                classExpression2, classExpression3 ), Collections.emptyList() );
+            classExpression2, classExpression3 ), Collections.emptyList() );
         final List<GraphElement> result = mapper.visit( axiom ).collect( Collectors.toList() );
 
         assertEquivalentResult( result, fooIri, barIri, bazIri );
--- src/test/java/de/atextor/owldiagram/TestIdentifierMapper.java
+++ src/test/java/de/atextor/owldiagram/TestIdentifierMapper.java
@@ -4,16 +4,22 @@ import de.atextor.owldiagram.graph.Node;
 import de.atextor.owldiagram.mappers.IdentifierMapper;
 import org.semanticweb.owlapi.model.IRI;
 
-import java.util.UUID;
+import java.util.Stack;
 
 public class TestIdentifierMapper implements IdentifierMapper {
+    private Stack<Node.Id> preconfiguredAnonIds = new Stack<>();
+
     @Override
     public Node.Id getIdForIri( final IRI iri ) {
         return new Node.Id( iri.getFragment() );
     }
 
+    void pushAnonId( final Node.Id id ) {
+        preconfiguredAnonIds.push( id );
+    }
+
     @Override
     public Node.Id getSyntheticId() {
-        return new Node.Id( UUID.randomUUID().toString() );
+        return preconfiguredAnonIds.pop();
     }
 }
