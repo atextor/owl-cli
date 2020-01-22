4ab8ca1a23ec05d42cc9671577f83603ca0c729b
Andreas Textor <textor.andreas@googlemail.com>
2020-01-22 04:55:11 +0100

    Add test for OWLDataOneOf

--- diagram/src/test/java/de/atextor/owlcli/diagram/OWLDataMapperTest.java
+++ diagram/src/test/java/de/atextor/owlcli/diagram/OWLDataMapperTest.java
@@ -1,7 +1,22 @@
 package de.atextor.owlcli.diagram;
 
+import de.atextor.owlcli.diagram.graph.Edge;
+import de.atextor.owlcli.diagram.graph.GraphElement;
+import de.atextor.owlcli.diagram.graph.Node;
+import de.atextor.owlcli.diagram.graph.NodeType;
 import de.atextor.owlcli.diagram.mappers.OWLDataMapper;
+import de.atextor.owlcli.diagram.mappers.Result;
 import org.junit.jupiter.api.Test;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLDataOneOf;
+import org.semanticweb.owlapi.model.OWLDataSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OWLDataMapperTest extends MapperTestBase {
     private final OWLDataMapper mapper = new OWLDataMapper( createTestMappingConfiguration() );
@@ -13,6 +28,43 @@ public class OWLDataMapperTest extends MapperTestBase {
 
     @Test
     public void testOWLDataOneOf() {
+        final String ontology = """
+            :name a owl:DatatypeProperty .
+            :Dog a owl:Class ;
+               owl:equivalentClass [
+                  a owl:Restriction ;
+                  owl:onProperty :name ;
+                  owl:someValuesFrom [
+                    a rdfs:Datatype ;
+                    owl:oneOf ( "Fido" "Bello" )
+                  ]
+               ] .
+            """;
+
+        final OWLEquivalentClassesAxiom axiom = getAxiom( ontology, AxiomType.EQUIVALENT_CLASSES );
+        final OWLDataSomeValuesFrom someValuesFrom = (OWLDataSomeValuesFrom) axiom.getOperandsAsList().get( 1 );
+
+        final OWLDataOneOf owlDataOneOf = (OWLDataOneOf) someValuesFrom.getFiller();
+        final String restrictionNodeId = "restrictionNode";
+        testIdentifierMapper.pushAnonId( new Node.Id( "Fido" ) );
+        testIdentifierMapper.pushAnonId( new Node.Id( "Bello" ) );
+        testIdentifierMapper.pushAnonId( new Node.Id( restrictionNodeId ) );
+
+        final Result result = mapper.visit( owlDataOneOf );
+        final Node restrictionNode = result.getNode();
+        assertThat( restrictionNode ).isInstanceOf( NodeType.ClosedClass.class );
+        final Set<GraphElement> remainingElements = result.getRemainingElements().collect( Collectors.toSet() );
+        assertThat( remainingElements ).isNotEmpty();
+
+        final List<Node> nodes = nodes( remainingElements );
+        assertThat( nodes ).hasSize( 2 );
+        assertThat( nodes ).anyMatch( isNodeWithId( "Fido" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "Bello" ) );
+
+        final List<Edge> edges = edges( remainingElements );
+        assertThat( edges ).hasSize( 2 );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( restrictionNodeId, "Fido" ) );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( restrictionNodeId, "Bello" ) );
     }
 
     @Test
