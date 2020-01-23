d05b80bf98e5067eba8cf8360788d565d265a2bd
Andreas Textor <textor.andreas@googlemail.com>
2020-01-23 05:14:16 +0100

    Add test for OWLDataUnionOf

--- diagram/src/test/java/de/atextor/owlcli/diagram/OWLDataMapperTest.java
+++ diagram/src/test/java/de/atextor/owlcli/diagram/OWLDataMapperTest.java
@@ -12,6 +12,7 @@ import org.semanticweb.owlapi.model.OWLDataComplementOf;
 import org.semanticweb.owlapi.model.OWLDataIntersectionOf;
 import org.semanticweb.owlapi.model.OWLDataOneOf;
 import org.semanticweb.owlapi.model.OWLDataSomeValuesFrom;
+import org.semanticweb.owlapi.model.OWLDataUnionOf;
 import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
 
 import java.util.List;
@@ -143,6 +144,41 @@ public class OWLDataMapperTest extends MapperTestBase {
 
     @Test
     public void testOWLDataUnionOf() {
+        final String ontology = """
+            :numberOfWings a owl:DatatypeProperty .
+            :Dog a owl:Class ;
+               owl:equivalentClass [
+                  a owl:Restriction ;
+                  owl:onProperty :numberOfWings ;
+                  owl:someValuesFrom [
+                    a rdfs:Datatype ;
+                    owl:unionOf ( xsd:nonNegativeInteger xsd:nonPositiveInteger )
+                  ]
+               ] .
+            """;
+
+        final OWLEquivalentClassesAxiom axiom = getAxiom( ontology, AxiomType.EQUIVALENT_CLASSES );
+        final OWLDataSomeValuesFrom someValuesFrom = (OWLDataSomeValuesFrom) axiom.getOperandsAsList().get( 1 );
+        final OWLDataUnionOf intersection = (OWLDataUnionOf) someValuesFrom.getFiller();
+
+        final String intersectionId = "intersectionNode";
+        testIdentifierMapper.pushAnonId( new Node.Id( intersectionId ) );
+
+        final Result result = mapper.visit( intersection );
+        assertThat( result.getNode().getClass() ).isEqualTo( NodeType.Union.class );
+        final Set<GraphElement> remainingElements = result.getRemainingElements().collect( Collectors.toSet() );
+        assertThat( remainingElements ).isNotEmpty();
+
+        final List<Node> nodes = nodes( remainingElements );
+        assertThat( nodes ).hasSize( 3 );
+        assertThat( nodes ).anyMatch( isNodeWithId( "nonNegativeInteger" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "nonPositiveInteger" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( intersectionId ) );
+
+        final List<Edge> edges = edges( remainingElements );
+        assertThat( edges ).hasSize( 2 );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( intersectionId, "nonNegativeInteger" ) );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( intersectionId, "nonPositiveInteger" ) );
     }
 
     @Test
