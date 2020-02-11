a993c4a232f2e9338a92c0ca68a319795f38a12b
Andreas Textor <textor.andreas@googlemail.com>
2020-02-11 05:50:00 +0100

    Add test for IRI in OWLAnnotationObjectMapperTest

--- diagram/src/test/java/de/atextor/owlcli/diagram/OWLAnnotationObjectMapperTest.java
+++ diagram/src/test/java/de/atextor/owlcli/diagram/OWLAnnotationObjectMapperTest.java
@@ -1,7 +1,11 @@
 package de.atextor.owlcli.diagram;
 
 import de.atextor.owlcli.diagram.mappers.OWLAnnotationObjectMapper;
+import de.atextor.owlcli.diagram.mappers.Result;
 import org.junit.jupiter.api.Test;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OWLAnnotationObjectMapperTest extends MapperTestBase {
     private final OWLAnnotationObjectMapper mapper = new OWLAnnotationObjectMapper( createTestMappingConfiguration() );
@@ -28,6 +32,17 @@ public class OWLAnnotationObjectMapperTest extends MapperTestBase {
 
     @Test
     public void testIRI() {
+        final String ontology = """
+                :comment a owl:AnnotationProperty .
+                :Dog a owl:Class ;
+                    :comment :Foo .
+                """;
+
+        final OWLAnnotationAssertionAxiom axiom = getAxiom( ontology, AxiomType.ANNOTATION_ASSERTION );
+        final Result result = mapper.visit( axiom.getValue().asIRI().get() );
+
+        assertThat( result.getNode() ).matches( isNodeWithId( "Foo" ) );
+        assertThat( result.getRemainingElements() ).isEmpty();
     }
 
     @Test
