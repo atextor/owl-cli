4073acef58dbfe8b8bc800eed5a653ba879ddcff
Andreas Textor <textor.andreas@googlemail.com>
2020-02-12 05:11:02 +0100

    Add test for OWLAnnotation

--- diagram/src/test/java/de/atextor/owlcli/diagram/OWLAnnotationObjectMapperTest.java
+++ diagram/src/test/java/de/atextor/owlcli/diagram/OWLAnnotationObjectMapperTest.java
@@ -1,10 +1,19 @@
 package de.atextor.owlcli.diagram;
 
+import de.atextor.owlcli.diagram.graph.Edge;
+import de.atextor.owlcli.diagram.graph.GraphElement;
+import de.atextor.owlcli.diagram.graph.Node;
+import de.atextor.owlcli.diagram.graph.NodeType;
 import de.atextor.owlcli.diagram.mappers.OWLAnnotationObjectMapper;
 import de.atextor.owlcli.diagram.mappers.Result;
 import org.junit.jupiter.api.Test;
 import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OWLAnnotationObjectMapperTest extends MapperTestBase {
@@ -12,6 +21,32 @@ public class OWLAnnotationObjectMapperTest extends MapperTestBase {
 
     @Test
     public void testOWLAnnotation() {
+        final String ontology = """
+                :comment a owl:AnnotationProperty .
+                :Dog a owl:Class ;
+                    :comment :Foo .
+                """;
+
+        final OWLAnnotationAssertionAxiom axiom = getAxiom( ontology, AxiomType.ANNOTATION_ASSERTION );
+        final Result result = mapper.visit( axiom.getAnnotation() );
+
+        assertThat( result.getNode().getClass() ).isEqualTo( NodeType.AnnotationRole.class );
+
+        final Set<GraphElement> remainingElements = result.getRemainingElements().collect( Collectors.toSet() );
+        assertThat( remainingElements ).isNotEmpty();
+
+        final List<Node> nodes = nodes( remainingElements );
+        assertThat( nodes ).hasSize( 1 );
+        final Node theNode = nodes.get( 0 );
+        assertThat( theNode.getClass() ).isEqualTo( NodeType.Literal.class );
+        final NodeType.Literal literal = (NodeType.Literal) theNode;
+
+        assertThat( literal.getId().getId() ).isEqualTo( "Foo" );
+        assertThat( literal.getValue() ).isEqualTo( "http://test.de/Foo" );
+
+        final List<Edge> edges = edges( remainingElements );
+        assertThat( edges ).hasSize( 1 );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( result.getNode().getId(), literal.getId() ) );
     }
 
     @Test
