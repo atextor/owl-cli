cd4aa00b593ba289dc7b7add2557244fe2d1b51d
Andreas Textor <textor.andreas@googlemail.com>
2020-04-09 05:54:34 +0200

    Add test for OWLAnnotationPropertyDomainAxiom with punning

--- diagram/src/test/java/de/atextor/owlcli/diagram/OWLOntologyMapperTest.java
+++ diagram/src/test/java/de/atextor/owlcli/diagram/OWLOntologyMapperTest.java
@@ -4,6 +4,7 @@ import de.atextor.owlcli.diagram.graph.DecoratedEdge;
 import de.atextor.owlcli.diagram.graph.Edge;
 import de.atextor.owlcli.diagram.graph.GraphElement;
 import de.atextor.owlcli.diagram.graph.Node;
+import de.atextor.owlcli.diagram.graph.NodeType;
 import de.atextor.owlcli.diagram.mappers.MappingConfiguration;
 import de.atextor.owlcli.diagram.mappers.OWLOntologyMapper;
 import org.junit.jupiter.api.Test;
@@ -28,7 +29,6 @@ public class OWLOntologyMapperTest extends MapperTestBase {
 
         final String iriReferenceId = "iriReference";
         testIdentifierMapper.pushAnonId( new Node.Id( iriReferenceId ) );
-        testIdentifierMapper.pushAnonId( new Node.Id( "asdf" ) );
 
         final Set<GraphElement> result = mapper.apply( createOntology( ontology ) ).collect( Collectors.toSet() );
 
@@ -41,6 +41,38 @@ public class OWLOntologyMapperTest extends MapperTestBase {
         assertThat( edges ).hasSize( 1 );
 
         final Edge propertyToDomain = edges.iterator().next();
+        assertThat( propertyToDomain.getType() ).isEqualTo( Edge.Type.DEFAULT_ARROW );
+        assertThat( propertyToDomain.getClass() ).isEqualTo( DecoratedEdge.class );
+        assertThat( ( (DecoratedEdge) propertyToDomain ).getDecoration() ).isEqualTo( DecoratedEdge.DOMAIN );
+    }
+
+    @Test
+    public void testOWLAnnotationPropertyDomainAxiomWithPunning() {
+        final String ontology = """
+            :foo a owl:Class .
+            :foo a owl:NamedIndividual .
+            :bar a owl:AnnotationProperty ;
+               rdfs:domain :foo .
+            """;
+
+        final String iriReferenceId = "iriReference";
+        testIdentifierMapper.pushAnonId( new Node.Id( iriReferenceId ) );
+
+        final Set<GraphElement> result = mapper.apply( createOntology( ontology ) ).collect( Collectors.toSet() );
+
+        final List<Node> nodes = nodes( result );
+        assertThat( nodes ).hasSize( 3 );
+        assertThat( nodes ).anyMatch( isNodeWithId( "foo" ).and( node -> node.is( NodeType.Class.class ) ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "foo" ).and( node -> node.is( NodeType.Individual.class ) ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "bar" ) );
+
+        final List<Edge> edges = edges( result );
+        assertThat( edges ).hasSize( 1 );
+
+        final Edge propertyToDomain = edges.iterator().next();
+        assertThat( propertyToDomain.getFrom().getId() ).isEqualTo( "bar" );
+        assertThat( propertyToDomain.getTo().getId() ).isEqualTo( "foo" );
+        assertThat( propertyToDomain.getTo().getIri() ).contains( iri( "foo" ) );
         assertThat( propertyToDomain.getType() ).isEqualTo( Edge.Type.DEFAULT_ARROW );
         assertThat( propertyToDomain.getClass() ).isEqualTo( DecoratedEdge.class );
         assertThat( ( (DecoratedEdge) propertyToDomain ).getDecoration() ).isEqualTo( DecoratedEdge.DOMAIN );
