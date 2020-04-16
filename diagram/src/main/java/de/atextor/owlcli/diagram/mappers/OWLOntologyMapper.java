48326c878116c3bb9a1ab81d6420192a6be79c36
Andreas Textor <textor.andreas@googlemail.com>
2020-04-16 07:06:00 +0200

    Properly handle punning

--- diagram/src/main/java/de/atextor/owlcli/diagram/mappers/OWLOntologyMapper.java
+++ diagram/src/main/java/de/atextor/owlcli/diagram/mappers/OWLOntologyMapper.java
@@ -1,14 +1,17 @@
 package de.atextor.owlcli.diagram.mappers;
 
+import com.google.common.collect.Sets;
 import de.atextor.owlcli.diagram.graph.Edge;
 import de.atextor.owlcli.diagram.graph.Graph;
 import de.atextor.owlcli.diagram.graph.GraphElement;
 import de.atextor.owlcli.diagram.graph.Node;
 import de.atextor.owlcli.diagram.graph.NodeType;
+import io.vavr.Tuple2;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 
-import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -21,87 +24,92 @@ public class OWLOntologyMapper implements Function<OWLOntology, Stream<GraphElem
         this.mappingConfiguration = mappingConfiguration;
     }
 
+    private Stream<GraphElement> resolveIriReferences( final Set<GraphElement> graph ) {
+        final Map<Edge, NodeType.IRIReference> references = getReferences( graph );
 
-    /**
-     * For an edge to an {@link NodeType.IRIReference}, find all nodes that have the IRI of the IRIReference as part of
-     * their id. Due to punning, this can actually be more than one node, e.g. there can be a {@link NodeType.Class}
-     * and a {@link NodeType.Individual} with the same IRI in their ids.
-     * Replace the edge to the IRIReference node with edges to each found referenced node.
-     * If no referenced node is found, replace the IRIReference with a Literal node containing the IRI and
-     * an updated edge to the literal.
-     *
-     * @param edge              the edge to an {@link NodeType.IRIReference}
-     * @param unchangedElements the graph elements that are not being replaced
-     * @param referenceElements the graph elements describing references, i.e. the IRIReferences and edges to them
-     * @return the updated referenceElements that contain no IRIReferences any more
-     */
-    private Stream<GraphElement> resolveEdgeToIriReference( final Edge edge,
-                                                            final List<GraphElement> unchangedElements,
-                                                            final List<GraphElement> referenceElements ) {
-        return referenceElements.stream()
-            .flatMap( element -> element.view( NodeType.IRIReference.class ) )
-            .flatMap( iriReference -> {
-                final Set<Node> referencedNodes = unchangedElements.stream()
-                    .filter( GraphElement::isNode )
-                    .map( GraphElement::asNode )
-                    .filter( node -> node.getId().getIri().map( iri -> iri.equals( iriReference.getIri() ) )
-                        .orElse( false ) )
-                    .collect( Collectors.toSet() );
-
-                if ( referencedNodes.isEmpty() ) {
-                    final NodeType.Literal iriLiteral = new NodeType.Literal(
-                        mappingConfiguration.getIdentifierMapper().getSyntheticId(),
-                        mappingConfiguration.getNameMapper().getName( iriReference.getIri() ) );
-                    return Stream.of( iriLiteral, edge.setTo( iriLiteral.getId() ) );
-                } else {
-                    return referencedNodes.stream()
-                        .map( referencedNode -> edge.setTo( referencedNode.getId() ) );
-                }
-            } );
-    }
-
-    /**
-     * Checks if a given {@link GraphElement} is a {@link NodeType.IRIReference} or the edge to one
-     *
-     * @param allElements all elements of the graph
-     * @param element     the element to check
-     * @return true if the element is a reference or the edge to one, otherwise false
-     */
-    private boolean isIriReferenceOrEdgeToIt( final Set<GraphElement> allElements, final GraphElement element ) {
-        if ( element.is( NodeType.IRIReference.class ) ) {
-            return true;
+        if ( references.isEmpty() ) {
+            return graph.stream();
         }
 
-        if ( element.isNode() ) {
-            return false;
-        }
-
-        final Edge edge = element.asEdge();
-        return allElements.stream()
-            .flatMap( e -> e.view( NodeType.IRIReference.class ) )
-            .anyMatch( e -> edge.getTo().equals( e.getId() ) );
+        final Set<GraphElement> resolvedReferences = resolveReferences( graph, references );
+        return mergeResolvedReferences( graph, references, resolvedReferences ).stream();
     }
 
-    /**
-     * Replace IRIReferences and the edges to them with edges to the referenced nodes, if such nodes exist.
-     * Otherwise turn the IRIReference into a Literal node.
-     *
-     * @param elements the elements on the graph that can contain {@link NodeType.IRIReference}s
-     * @return elements of the graph where all references are resolved
-     */
-    private Stream<GraphElement> resolveIriReferences( final Set<GraphElement> elements ) {
-        final Map<Boolean, List<GraphElement>> map = elements.stream()
-            .collect( Collectors.partitioningBy( element -> isIriReferenceOrEdgeToIt( elements, element ) ) );
+    private Set<GraphElement> mergeResolvedReferences( final Set<GraphElement> graph,
+                                                       final Map<Edge, NodeType.IRIReference> references,
+                                                       final Set<GraphElement> resolvedReferences ) {
+        final Set<GraphElement> elementsToRemove = references.entrySet().stream().flatMap( entry -> {
+            final Edge edge = entry.getKey();
+            final NodeType.IRIReference reference = entry.getValue();
+            final Stream<Node> referencedNodes = findNodesWithIri( graph, reference.getIri() );
+            return Stream.concat( Stream.of( edge, reference ), referencedNodes );
+        } ).collect( Collectors.toSet() );
+        final Set<GraphElement> graphWithoutReferences = Sets.difference( graph, elementsToRemove );
+        return Sets.union( graphWithoutReferences, resolvedReferences );
+    }
 
-        final List<GraphElement> unchangedElements = map.get( false );
-        final List<GraphElement> referenceElements = map.get( true );
+    private Set<GraphElement> resolveReferences( final Set<GraphElement> graph,
+                                                 final Map<Edge, NodeType.IRIReference> referenceMap ) {
+        return referenceMap.entrySet().stream().flatMap( entry -> {
+            final Edge edge = entry.getKey();
+            final NodeType.IRIReference reference = entry.getValue();
+            final Set<Node> referencedNodes =
+                findNodesWithIri( graph, reference.getIri() ).collect( Collectors.toSet() );
 
-        final Stream<GraphElement> updatedElements = referenceElements.stream()
+            if ( referencedNodes.isEmpty() ) {
+                final Node literal = turnReferenceIntoLiteral( reference );
+                return Stream.of( edge.setTo( literal.getId() ), literal );
+            }
+
+            return updateReferenceElements( edge, referencedNodes );
+        } ).collect( Collectors.toSet() );
+    }
+
+    private Stream<GraphElement> updateReferenceElements( final Edge edge, final Set<Node> referencedNodes ) {
+        return referencedNodes.stream().flatMap( node -> {
+            final Stream<Node> classView = node.view( NodeType.Class.class ).map( classNode ->
+                new NodeType.Class( buildNewNodeId( classNode.getId() ), classNode.getName() ) );
+            final Stream<Node> individualView = node.view( NodeType.Individual.class ).map( indivualNode ->
+                new NodeType.Individual( buildNewNodeId( indivualNode.getId() ), indivualNode.getName() ) );
+
+            return Stream.concat( classView, individualView )
+                .flatMap( updatedNode -> Stream.of( edge.setTo( updatedNode.getId() ), updatedNode ) );
+        } );
+    }
+
+    private Node.Id buildNewNodeId( final Node.Id original ) {
+        return original.getIri().map( iri ->
+            mappingConfiguration.getIdentifierMapper().getSyntheticIdForIri( iri ) )
+            .orElseGet( () -> mappingConfiguration.getIdentifierMapper().getSyntheticId() );
+    }
+
+    private NodeType.Literal turnReferenceIntoLiteral( final NodeType.IRIReference reference ) {
+        return new NodeType.Literal(
+            mappingConfiguration.getIdentifierMapper().getSyntheticId(),
+            mappingConfiguration.getNameMapper().getName( reference.getIri() ) );
+    }
+
+    private Stream<Node> findNodesWithIri( final Set<GraphElement> graph, final IRI iri ) {
+        return graph.stream()
+            .filter( GraphElement::isNode )
+            .map( GraphElement::asNode )
+            .filter( node -> node.getId().getIri().map( nodeIri -> nodeIri.equals( iri ) ).orElse( false ) );
+    }
+
+    private Map<Edge, NodeType.IRIReference> getReferences( final Set<GraphElement> graph ) {
+        return graph.stream()
             .filter( GraphElement::isEdge )
             .map( GraphElement::asEdge )
-            .flatMap( edge -> resolveEdgeToIriReference( edge, unchangedElements, referenceElements ) );
+            .flatMap( edge -> getReferenceThatEdgeLeadsTo( graph, edge ).map( node -> new Tuple2<>( edge, node ) ).stream() )
+            .collect( Collectors.toMap( tuple -> tuple._1, tuple -> tuple._2 ) );
+    }
 
-        return Stream.concat( unchangedElements.stream(), updatedElements );
+    private Optional<NodeType.IRIReference> getReferenceThatEdgeLeadsTo( final Set<GraphElement> graph,
+                                                                         final Edge edge ) {
+        return graph.stream()
+            .flatMap( element -> element.view( NodeType.IRIReference.class ) )
+            .filter( reference -> reference.getId().equals( edge.getTo() ) )
+            .findAny();
     }
 
     @Override
@@ -110,8 +118,6 @@ public class OWLOntologyMapper implements Function<OWLOntology, Stream<GraphElem
             .map( axiom -> axiom.accept( mappingConfiguration.getOwlAxiomMapper() ) )
             .flatMap( Graph::toStream )
             .collect( Collectors.toSet() );
-
         return resolveIriReferences( elements );
     }
-
 }
