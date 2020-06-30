0b7d8d48507a34de5c998b422c6ec59bb7eefd26
Andreas Textor <textor.andreas@googlemail.com>
2020-06-30 05:19:15 +0200

    Add test for SWRL Class atoms with expressions

--- diagram/src/test/java/de/atextor/owlcli/diagram/SWRLObjectMapperTest.java
+++ diagram/src/test/java/de/atextor/owlcli/diagram/SWRLObjectMapperTest.java
@@ -18,6 +18,7 @@ package de.atextor.owlcli.diagram;
 import de.atextor.owlcli.diagram.graph.Edge;
 import de.atextor.owlcli.diagram.graph.GraphElement;
 import de.atextor.owlcli.diagram.graph.Node;
+import de.atextor.owlcli.diagram.graph.node.ObjectQualifiedMinimalCardinality;
 import de.atextor.owlcli.diagram.graph.node.Rule;
 import de.atextor.owlcli.diagram.mappers.MappingConfiguration;
 import de.atextor.owlcli.diagram.mappers.OWLAxiomMapper;
@@ -122,7 +123,6 @@ public class SWRLObjectMapperTest extends MapperTestBase {
         final SWRLRule rule = getAxiom( ontology, AxiomType.SWRL_RULE );
 
         final Set<GraphElement> result = rule.accept( mapper ).getElementSet();
-        System.out.println( result );
 
         final List<Node> nodes = nodes( result );
         assertThat( nodes ).hasSize( 3 );
@@ -185,7 +185,6 @@ public class SWRLObjectMapperTest extends MapperTestBase {
         final SWRLRule rule = getAxiom( ontology, AxiomType.SWRL_RULE );
 
         final Set<GraphElement> result = rule.accept( mapper ).getElementSet();
-        System.out.println( result );
 
         final List<Node> nodes = nodes( result );
         assertThat( nodes ).hasSize( 4 );
@@ -201,5 +200,69 @@ public class SWRLObjectMapperTest extends MapperTestBase {
         assertThat( edges ).anyMatch( isEdgeWithFromAndTo( ruleNode.getId().getId(), "Adult" ) );
         assertThat( edges ).anyMatch( isEdgeWithFromAndTo( ruleNode.getId().getId(), "Person" ) );
         assertThat( edges ).anyMatch( isEdgeWithFromAndTo( ruleNode.getId().getId(), "age" ) );
+    }
+
+    @Test
+    public void testSWRLRuleWithClassAtomWithExpression() {
+        final String ontology = """
+            :Person a owl:Class .
+            :Parent a owl:Class .
+
+            :hasChild a owl:ObjectProperty .
+
+            var:p a swrl:Variable .
+
+            [
+               a swrl:Imp ;
+               swrl:body (
+                  [
+                     a swrl:ClassAtom ;
+                     swrl:classPredicate :Person ;
+                     swrl:argument1 var:p
+                  ]
+                  [
+                     a swrl:ClassAtom ;
+                     swrl:classPredicate [
+                        a owl:Restriction ;
+                        owl:onProperty :hasChild ;
+                        owl:minQualifiedCardinality "1"^^xsd:nonNegativeInteger ;
+                        owl:onClass :Person
+                     ] ;
+                     swrl:argument1 var:p
+                  ]
+               );
+               swrl:head (
+                  [
+                     a swrl:ClassAtom ;
+                     swrl:classPredicate :Parent ;
+                     swrl:argument1 var:p
+                  ]
+               )
+            ] .
+            """;
+
+        final SWRLRule rule = getAxiom( ontology, AxiomType.SWRL_RULE );
+
+        final Set<GraphElement> result = rule.accept( mapper ).getElementSet();
+
+        final List<Node> nodes = nodes( result );
+        assertThat( nodes ).hasSize( 5 );
+        assertThat( nodes ).anyMatch( node -> node.is( Rule.class ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "Person" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "Parent" ) );
+        assertThat( nodes ).anyMatch( isNodeWithId( "hasChild" ) );
+        assertThat( nodes ).anyMatch( node -> node.is( ObjectQualifiedMinimalCardinality.class ) );
+
+        final Node ruleNode = nodes.stream().filter( node -> node.is( Rule.class ) ).findFirst().get();
+
+        final List<Edge> edges = edges( result );
+        final Node cardinality =
+            nodes.stream().filter( node -> node.is( ObjectQualifiedMinimalCardinality.class ) ).findFirst().get();
+        assertThat( edges ).hasSize( 5 );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( ruleNode.getId().getId(), "Person" ) );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( ruleNode.getId().getId(), "Parent" ) );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( ruleNode.getId().getId(), cardinality.getId().getId() ) );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( cardinality.getId().getId(), "hasChild" ) );
+        assertThat( edges ).anyMatch( isEdgeWithFromAndTo( cardinality.getId().getId(), "Person" ) );
     }
 }
