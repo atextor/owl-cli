97cc8c11a45d998fdc2920564c26f131056f0aa8
Andreas Textor <textor.andreas@googlemail.com>
2021-06-17 16:05:55 +0200

    Set up integration test

---
+++ cli/src/integrationTest/java/de/atextor/owlcli/BinaryIntegrationTest.java
@@ -0,0 +1,28 @@
+/*
+ * This file is part of OWL-CLI.
+ * The contents of this file are subject to the LGPL License, Version 3.0.
+ * Copyright (c) 2021, Andreas Textor.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see http://www
+ * .gnu.org/licenses/.
+ */
+
+package de.atextor.owlcli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BinaryIntegrationTest {
+
+    @Test
+    public void testBinary() {
+        assertThat( true ).isEqualTo( true );
+    }
+}
